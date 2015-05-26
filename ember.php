df949b8c7949ee8feda3978a90832c6cdf838a81
vagrant <vagrant@scotchbox>
2015-05-26 14:18:13 +0000

    Started work on more responsive layout for ember.js frontend

---
+++ ember.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Rummy">
+    <meta name="author" content="Mat Gilbert">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>Rummy</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+    <link href="css/ember.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+	  
+    <div class="container-fluid">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Start New Game</a></li>
+            <li role="presentation"><a href="#">P1</a></li>
+            <li role="presentation"><a href="#">P2</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Rummy</h3>
+      </div>
+      
+    </div>
+<div class="container-fluid">
+ 
+<div class="row row-centered">
+	<div class="col-xs-6">
+	Discard
+
+	</div>
+	<div class="col-xs-6">
+	Stack	
+		
+	</div>	
+	
+</div>	 
+
+    <div class="row row-centered">
+
+        <div class="col-xs-5 col-centered cardLeftSide">
+		        
+		        <div class="card">AS
+			        <button class=" btn btnDiscard">Discard</button>
+		        </div>
+		        <div class="card">KS
+			        <button class=" btn btnDiscard">Discard</button>
+		        </div>
+		        <div class="card">JS
+			        <button class=" btn btnDiscard">Discard</button>    
+		        </div>
+
+        </div>
+        <div class="col-xs-7 col-centered cardRightSide">
+
+		        <div class="card">10S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">9S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">8S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">7S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>		        
+		        		    	
+
+	    </div>   
+
+    </div>    
+   
+    <div class="row row-centered">
+
+        <div class="col-xs-5 col-centered cardLeftSide">
+		        
+		        <div class="card">AS
+			        <button class=" btn btnDiscard">Discard</button>
+		        </div>
+		        <div class="card">KS
+			        <button class=" btn btnDiscard">Discard</button>
+		        </div>
+		        <div class="card">JS
+			        <button class=" btn btnDiscard">Discard</button>    
+		        </div>
+
+        </div>
+        <div class="col-xs-7 col-centered cardRightSide">
+
+		        <div class="card">10S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">9S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">8S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>
+		        <div class="card">7S
+			        <button class=" btn btnDiscard">Discard</button>			        
+		        </div>		        
+		        		    	
+
+	    </div>   
+
+    </div>    
+</div>
+
+
+      <footer class="footer">
+        <p>&copy; Mat Gilbert 2015</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </body>
+</html>
